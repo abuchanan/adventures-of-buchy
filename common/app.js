619db5fdc61183706f3957587c8da19a9ebb6834
Alex Buchanan <buchanae@gmail.com>
2016-06-05 03:00:43 +1000

    Fix pageview analytics

--- common/app.js
+++ common/app.js
@@ -10,6 +10,13 @@ const history = browserHistory;
 const mountNode = document.getElementById('container');
 const routes = getRoutes(true).routes;
 
+history.listen(location => {
+  if (window.ga !== undefined) {
+    window.ga("set", "page", location.pathname);
+    window.ga("send", "pageview");
+  }
+});
+
 match({ history, routes }, (error, redirectLocation, renderProps) => {
   ReactDOM.render(<Router {...renderProps} />, mountNode)
 });
